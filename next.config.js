9ad629bed92985ed77c08aa602995adb23c85df3
sohel-tech4 <soheltech4@gmail.com>
2025-01-24 08:41:26 +0600

    add folder structure

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const nextConfig = {
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "./components/page/home/Banner";
+
+const CommonPage = () => {
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
+};
+
+export default CommonPage;
