4529f2b663754a47f0b3e9214b83181bb2d72d97
sohel-tech4 <soheltech4@gmail.com>
2025-01-25 01:15:53 +0600

    correction navbar

--- src/app/(commonLayout)/components/Shaared/Navbar/Navbar.tsx
+++ src/app/(commonLayout)/components/Shaared/Navbar/Navbar.tsx
@@ -1,81 +1,56 @@
 import React from "react";
 
+import Link from "next/link";
+
 const Navbar = () => {
+  const Nav = (
+    <>
+      <Link href="/">Home</Link>
+      <Link href="/dashboard">Dashboard</Link>
+    </>
+  );
+
   return (
-    <div className="navbar bg-base-100 shadow-md justify-between">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="bg-base-100 flex shadow-md justify-between">
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] gap-5 mt-3 w-52 p-2 shadow"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              {Nav}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <div className="flex gap-2 items-center">
+            <img
+              className="md:w-12 md:h-11 h-5 w-6"
+              src="https://i.ibb.co.com/Tq6jPbR/snapbasket.png"
+            ></img>
+            <h1 className="md:uppercase md:font-bold md:text-4xl bg-[#1E88E5] text-white px-1">
+              Snap Basket
+            </h1>
+          </div>
         </div>
-        <div className="flex gap-2 items-center">
-          <img
-            className="md:w-12 md:h-11 h-5 w-6"
-            src="https://i.ibb.co.com/Tq6jPbR/snapbasket.png"
-          ></img>
-          <h1 className="md:uppercase md:font-bold md:text-4xl bg-[#1E88E5] text-white px-1">
-            Snap Basket
-          </h1>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-5 ">{Nav}</ul>
         </div>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
       </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
