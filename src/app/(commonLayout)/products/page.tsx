5bb8667c045d6b318625b02542ab63673bf200c3
Sohel Rana <soheltech4@gmail.com>
2025-01-28 13:28:57 +0600

    add card

--- src/app/(commonLayout)/products/page.tsx
+++ src/app/(commonLayout)/products/page.tsx
@@ -2,23 +2,33 @@ import Image from "next/image";
 
 const Products = async () => {
   const res = await fetch("http://localhost:5000/api/v1/products");
-  const {data} = await res.json()
+  const { data } = await res.json();
 
-  
+  console.log(data);
+
   return (
-    <div className="card glass w-96">
-      {/* <figure>
-        <Image src={data?.images} height={50} width={50}></Image>
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{data.name}</h2>
-        <p>How to park your car at your garage?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Learn now!</button>
+   <div className="my-5">
+    <h1 className="text-center my-10 text-3xl font-bold">All Products</h1>
+     <div className="flex flex-wrap gap-4 justify-center">
+      {data?.map((item: any) => (
+        <div key={item?._id} className="card glass w-96">
+          <figure>
+            <img
+              src={item?.images}
+              alt="car!"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{item?.name}</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
         </div>
-      </div> */}
+      ))}
     </div>
-    // <></>
+   </div>
   );
 };
 
