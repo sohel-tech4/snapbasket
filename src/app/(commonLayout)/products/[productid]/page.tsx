ad96ecb703db20425748241a3246f960ea79a938
sohel-tech4 <soheltech4@gmail.com>
2025-01-28 16:59:48 +0600

    update

---
+++ src/app/(commonLayout)/products/[productid]/page.tsx
@@ -0,0 +1,37 @@
+const ProductsDetailsPage = async ({
+  params,
+}: {
+  params: { productid: string };
+}) => {
+  const res = await fetch(
+    `http://localhost:5000/api/v1/products/${params.productid}`
+  );
+  const { data } = await res.json();
+
+  const { name, description, price, stock, category, images } = data;
+
+  return (
+    <div className="md:p-10 md:py-10">
+      <div className="flex flex-col md:flex-row gap-6">
+        <img src={images} alt={name} className="md:w-1/2 rounded-lg p-2" />
+
+        <div className="md:w-2/4 p-2">
+          <h1 className="text-3xl font-bold text-gray-800 mb-2">{name}</h1>
+          <p className=" text-xl mb-4">{description}</p>
+          <p className="text-2xl font-semibold text-green-600 mb-4">
+            Price: ${price}
+          </p>
+          <p className="text-xl  mb-2">Category: {category}</p>
+          <div className="flex items-center justify-between">
+            <p className="text-xl  mb-2">Stock: {stock}</p>
+            <button className="bg-[#FA552F] btn btn-md text-white hover:bg-[#1E88E5] hover:text-white ">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsDetailsPage;
--- src/app/(commonLayout)/products/page.tsx
+++ src/app/(commonLayout)/products/page.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 const Products = async () => {
   const res = await fetch("http://localhost:5000/api/v1/products");
@@ -7,30 +7,38 @@ const Products = async () => {
   // console.log(data);
 
   return (
-   <div className="my-5">
-    <h1 className="text-center my-10 text-3xl font-bold">All Products</h1>
-     <div className="flex flex-wrap gap-4 justify-center ">
-      {data?.map((item: any) => (
-        <div key={item?._id} className="card glass w-96 hover:scale-105 transition-transform duration-300">
-          <figure>
-            <img
-              src={item?.images}
-              alt="car!"
-              className="hover:scale-105 transition-transform duration-300"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{item?.name}</h2>
-            <p>{item?.description}</p>
-            <div className="card-actions flex justify-center items-center">
-              <p>Price: {item?.price}$</p>
-              <Link to={`${item?._id}`} className="bg-[#FA552F] btn btn-md text-white hover:bg-[#1E88E5] hover:text-white ">Shop Now</Link>
+    <div className="my-5">
+      <h1 className="text-center my-10 text-3xl font-bold">All Products</h1>
+      <div className="flex flex-wrap gap-4 justify-center ">
+        {data?.map((item: any) => (
+          <div
+            key={item?._id}
+            className="card glass w-96 hover:scale-105 transition-transform duration-300"
+          >
+            <figure>
+              <img
+                src={item?.images}
+                alt="car!"
+                className="hover:scale-105 transition-transform duration-300"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{item?.name}</h2>
+              <p>{item?.description}</p>
+              <div className="card-actions flex justify-center items-center">
+                <p>Price: {item?.price}$</p>
+                <Link
+                  href={`/products/${item?._id}`}
+                  className="bg-[#FA552F] btn btn-md text-white hover:bg-[#1E88E5] hover:text-white "
+                >
+                  View Details
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-   </div>
   );
 };
 
